b40fcaeb910b631ec78cbd23843573135a383eca
khlap <khlaponin.dp@gmail.com>
2022-01-15 20:09:29 +0100

    2. Сокобан. Тестовое задание. [#75283]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,7 +2,13 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
-        boolean rsl = true;
-        return rsl;
+        boolean result = false;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][i] == 1) {
+                result = true;
+                break;
+            }
+        }
+        return result;
     }
 }
